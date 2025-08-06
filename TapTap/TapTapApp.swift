cea7a484bf6425a8679e49e30eb3b4f201640be9
HoangVu123456 <dkhv1508@gmail.com>
2025-08-06 10:39:02 +0800

    Initial Commit

---
+++ TapTap/TapTapApp.swift
@@ -0,0 +1,18 @@
+//
+//  TapTapApp.swift
+//  TapTap
+//
+//  Created by Interactive 3D Design on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TapTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
