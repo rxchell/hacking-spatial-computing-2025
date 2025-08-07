f642e59e294340748c7c051ccdc1f121a8d453f1
Yuchi Chen <mp32352@gmail.com>
2025-08-07 22:03:05 +0800

    - Add TextStyle.swift

--- TapTap/Instruction.swift
+++ TapTap/Instruction.swift
@@ -210,23 +210,6 @@ struct ResultsView: View {
     }
 }
 
-extension Text {
-    func resultLabelStyle() -> some View {
-        self
-            .font(.system(size: 50, weight: .bold, design: .rounded))
-    }
-    
-    func resultValueStyle() -> some View {
-        self
-            .font(.system(size: 50, weight: .bold, design: .rounded))
-            .foregroundColor(.blue)
-            .padding(.vertical, 10)
-            .padding(.horizontal, 20)
-            .background(Color.white)
-            .cornerRadius(16)
-    }
-}
-
 #Preview(windowStyle: .volumetric) {
     Instruction()
 }
