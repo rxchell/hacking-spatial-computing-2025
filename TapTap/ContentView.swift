f4a84e4ec2c2bf4dfce38b4dc64879c324667071
HoangVu123456 <dkhv1508@gmail.com>
2025-08-07 11:05:02 +0800

    Bubbles features first version (slow popping problem)

--- TapTap/ContentView.swift
+++ TapTap/ContentView.swift
@@ -12,6 +12,9 @@ import RealityKitContent
 struct ContentView: View {
 
     @State var enlarge = false
+    @State var tapped = false
+    @State var tappedName = ""
+    @State var orbDict: [String: Entity] = [:]
 
     var body: some View {
         VStack{
@@ -26,15 +29,42 @@ struct ContentView: View {
                         if let orb = orbScene.findEntity(named: "Sphere") {
                             while true {
                                 let orbClone = orb.clone(recursive: true)
+                                let id = UUID().uuidString
+                                orbClone.name = "Orb-\(id)"
+                                orbDict[id] = orbClone
                                 
                                 let x = Float.random(in: 0...1)
                                 let y = Float.random(in: 1...2)
                                 
-                                
                                 orbClone.position = SIMD3(x, y, -1)
                                 
+                                orbClone.components.set(CollisionComponent(shapes: [.generateSphere(radius: 1)]))
+                                orbClone.components.set(InputTargetComponent())
+                                
+                                
+                                Task.detached {
+                                    try? await Task.sleep(for: .seconds(1000))
+                                    
+                                    await MainActor.run {
+                                                                orbClone.removeFromParent()
+                                                            }
+                                }
+                                
+                                Task.detached {
+                                    if (tapped == true) {
+                                        for (_, orb) in orbDict {
+                                            if (orb.name == tappedName) {
+                                                await orb.removeFromParent()
+                                                print("Execute")
+                                            }
+                                        }
+                                    }
+                                    tapped = false
+                                    tappedName = ""
+                                }
+                                
                                 anchor.addChild(orbClone)
-                                try? await Task.sleep(for: .seconds(2))
+                                try? await Task.sleep(for: .seconds(6))
                             }
                         } else {
                             print("Sphere entity not found inside OrbScene")
@@ -43,7 +73,11 @@ struct ContentView: View {
                         print("Failed to load OrbScene: \(error)")
                     }
                 }
-            }
+            }.gesture(TapGesture().targetedToAnyEntity().onEnded { value in
+                    print("Entity tapped! \(value)")
+                tappedName = value.entity.name
+                    tapped = true
+            })
         }
     }
 }
